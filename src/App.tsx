67052ed907364580f2b77b04fbcdbe0e760fceb2
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2024-11-20 18:36:46 +0200

    clone react_todo_app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,69 @@
-import React from 'react';
-import './App.scss';
+import { FC } from 'react';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { ErrorNotification } from './components/ErrorNotification';
+import { Header } from './components/Header';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { useTodo } from './Hooks/useTodo';
 
-export const App: React.FC = () => {
+export const App: FC = () => {
+  const {
+    todos,
+    tempTodo,
+    error,
+    idsForDelete,
+    todosForUpdate,
+    filterOption,
+    completedTodosId,
+    numberOfActiveTodos,
+    filteredTodos,
+    setTodos,
+    setTempTodo,
+    setFilterOption,
+    handleSetError,
+    handleDeleteTodos,
+    handleChangeTodos,
+    hideError,
+  } = useTodo();
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header
+          todos={todos}
+          tempTodo={tempTodo}
+          onSetError={handleSetError}
+          setTodos={setTodos}
+          setTempTodo={setTempTodo}
+          onTodosChange={handleChangeTodos}
+        />
+
+        {(!!todos.length || tempTodo) && (
+          <>
+            <TodoList
+              todos={filteredTodos}
+              tempTodo={tempTodo}
+              todosForUpdate={todosForUpdate}
+              onTodosChange={handleChangeTodos}
+              idsForDelete={idsForDelete}
+              onDeleteTodo={handleDeleteTodos}
+              error={error}
+            />
+            <Footer
+              filterOption={filterOption}
+              numberOfActiveTodos={numberOfActiveTodos}
+              completedTodosId={completedTodosId}
+              setFilterOption={setFilterOption}
+              onDeleteTodos={handleDeleteTodos}
+            />
+          </>
+        )}
+      </div>
+
+      <ErrorNotification error={error} hideError={hideError} />
     </div>
   );
 };
